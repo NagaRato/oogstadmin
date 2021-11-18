a7fc4650ece5d5af75e6fd719d9324714eb66af1
JavanKadd <kai.herwijn@additude.eu>
2021-11-18 20:58:51 +0100

    Initial commit

---
+++ src/controller/MainController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.swing.*;
+
+public class MainController {
+    public MainController() {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(1200, 800);
+        mainFrame.setTitle("OogstAdmin");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+}
---
+++ src/controller/setup.java
@@ -0,0 +1,10 @@
+package controller;
+
+import controller.MainController;
+
+public class setup {
+
+    public static void main(String[] args) {
+        MainController mc = new MainController();
+    }
+}
